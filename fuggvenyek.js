d246180ad1fff7bb6b247ac1261a41ec03c88957
Barni <tothbarni.06@gmail.com>
2025-02-27 12:19:54 +0100

    orai

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,19 +1,30 @@
-let articleElem = document.getElementById("art")
+import { kocsiLista } from "./adat.js";
+
+let articleElem = document.getElementById("art");
 
 export function egyAutoMegjelenit(lista, index) {
-    articleElem.innerHTML+=`<div class="modellek">
-                               <h3>${lista[index].nev}</h3>
-                               <img src="${lista[index].kep}" alt="skoda">
-                               <p>${lista[index].ar}</p>
+    const auto = lista[index];
+    articleElem.innerHTML += `<div class="modellek">
+                               <h3>${auto.nev}</h3>
+                               <img src="${auto.kep}" alt="skoda">
+                               <p>${auto.ar}</p>
                                <div class="gombok">
-                                   <button>Konfigurálás</button>
+                                   <button data-index="${index}" class="konfGomb">Konfigurálás</button>
                                    <button>Rendelés</button>
                                </div>
-                           </div>`
+                           </div>`;
 }
 
-export function autoMegjelenit(lista){
+export function autoMegjelenit(lista) {
     for (let index = 0; index < lista.length; index++) {
-        egyAutoMegjelenit(lista, index)
+        egyAutoMegjelenit(lista, index);
     }
+    const konfGombok = document.querySelectorAll(".konfGomb");
+    for (let i = 0; i < konfGombok.length; i++) {
+        konfGombok[i].addEventListener("click", function() {
+            const auto = lista[konfGombok[i].dataset.index];
+            console.log("Kocsi neve: " + auto.nev + "\nKép URL: " + auto.kep + "\nÁr: " + auto.ar);
+        });
+    }
+    
 }
